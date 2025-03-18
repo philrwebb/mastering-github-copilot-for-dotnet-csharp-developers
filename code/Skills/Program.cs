1ecc42d59961cfc2e4feff577a71a0c55830c8a5
Philip Webb <prwebb@optusnet.com.au>
2025-03-18 07:17:36 +0000

    add Skills project and implement basic arithmetic operations

---
+++ code/Skills/Program.cs
@@ -0,0 +1,7 @@
+﻿void CalculateNumbers(int var1, int var2)
+{
+    Console.WriteLine($"Sum: {var1 + var2}");
+    Console.WriteLine($"Difference: {var1 - var2}");
+    Console.WriteLine($"Product: {var1 * var2}");
+    Console.WriteLine($"Quotient: {var1 / var2}");
+}
