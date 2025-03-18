e505d397591914f5d956c924ce0db1573afdd04c
Philip Webb <prwebb@optusnet.com.au>
2025-03-18 07:24:09 +0000

    add Members class with methods to retrieve member name and email

---
+++ code/Skills/Members.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Skills;
+
+public class Members
+{
+    public string GetMemberName()
+    {
+        return "John Doe";
+    }
+
+    public string GetMemberEmail()
+    {
+        return "jdoe@gmail.com";
+    }
+}
